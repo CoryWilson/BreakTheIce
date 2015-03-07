5f0f5864f037cfaf7e246436b96a698b325131d7
CoryWilson <ctwilson08@gmail.com>
2015-03-07 14:12:02 -0500

    typo fix route locationSearch

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ connection.connect(function(err) {
     console.log('connected as id ' + connection.threadId);
 });
 
-router.get('/locationSearch',function(req,res)){
+router.get('/locationSearch',function(req,res){
 
     geocoder.geocode('29 champs elysée paris', function(err, res) {
         console.log(res);
